abc5072cba0d7aed55b62278542966a35c1dafde
Muhammad Fayaz <fayazjani7576@gmail.com>
2025-04-25 11:18:06 +0500

    chat-1

---
+++ src/app/(Main)/page.tsx
@@ -0,0 +1,28 @@
+import FeatureSection from "@/components/pages/FeatureSection";
+import Footer from "@/components/pages/Footer";
+import HeroSection from "@/components/pages/Hero-section";
+import Navbar from "@/components/pages/Navbar";
+import UserReviews from "@/components/pages/UserReviews";
+import React from "react";
+
+const MainUi = () => {
+  return (
+    <div className="w-full min-h-screen bg-gray-100  flex flex-col items-center justify-center">
+      {/* Header */}
+      <Navbar />
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default MainUi;
